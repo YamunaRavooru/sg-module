8671adf0bcb9c9f05ed3f32b1df77eaba53b501e
yamuna <malini.mahathi@gmail.com>
2025-02-06 17:23:43 +0530

    sg module

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable project{
+
+}
+variable environment{
+
+}
+variable sg_name{
+
+}
+variable sg_description{
+  
+}
+variable vpc_id{
+
+}
+variable sg_tags{
+    default={}
+}
+variable common_tags{
+    type=map
+}
